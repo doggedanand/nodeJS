9b7b23bdcbf0764b698b97336e27aa1cac4dbfa6
anand <rd.anandv@gmail.com>
2023-10-21 21:09:39 +0530

    write and read the JSON file

---
+++ practice/g/test/test.js
@@ -0,0 +1,14 @@
+[
+  {
+    name: "Alice",
+    age: 25,
+    city: "Wonderland",
+    isStudent: false,
+    grades: [90, 85, 92],
+    address: {
+      street: "123 Main St",
+      city: "Example City",
+      zipCode: "12345",
+    },
+  },
+];
